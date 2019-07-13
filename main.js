a5dd0b7cb47e2973cac49ed6cd133a6d4b478adc
Dg <dcabrantesm@gmail.com>
2019-07-13 22:55:27 +0100

    v2 fix bot limiting

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ var roleUpgrader = require('role.upgrader');
 var rndName = require('rnd.name');
 
 var lastCreep = 0;
-
+/*
+Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], "name",{memory: {role: "harvester"}} )
+*/
 module.exports.loop = function () {
-
-    if(Game.spawns['Spawn1'].energy>=300 && Game.creeps <= 8){
+    
+    
+    if(Game.spawns['Spawn1'].energy>=300 && Object.keys(Game.creeps).length <= 8){
         var name;
         var role;
         switch (lastCreep) {
@@ -39,17 +42,28 @@ module.exports.loop = function () {
                 }
             } 
             );
+    }else if(Object.keys(Game.creeps).length >= 8){
+        //Apagar Robos mortos
+        for(var i in Memory.creeps) {
+            if(!Game.creeps[i] || Game.creeps[i].ticksToLive < 2) {
+                delete Memory.creeps[i];
+            }
+        }
     }
+
+    console.log("SPAWN",Game.spawns['Spawn1'].energy,Object.keys(Game.creeps).length)
     for(var name in Game.creeps) {
+        console.log(name+" "+Game.creeps[name].ticksToLive);
+        
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
+            //roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+            //roleBuilder.run(creep);
         }
     }
 }
